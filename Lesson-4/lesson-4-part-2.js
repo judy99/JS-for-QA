acb5cc470bebd9e8ef8b7f59b1d5ce896d6b92e0
Julia Kurianova <julia.kurianova@gmail.com>
2021-03-19 19:41:31 -0400

    Add Lesson-4 part-1

---
+++ Lesson-4/lesson-4-part-2.js
@@ -0,0 +1,74 @@
+// Exercise 1
+// Write a function which should return sum of all numbers inside array
+const numbers = [1, -1, 2, 3];
+
+function sum(numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum = sum + numbers[i]
+    }
+    return sum
+}
+
+console.log(sum(numbers)); // 5
+
+// console.log(numbers.reduce((sum, number) => sum + number))
+
+// Exercise 2
+// When you run arrayFromRange function should dispayd numbers from min to max 
+// Also this function should accept negative numbers
+function arrayFromRange(min, max) {
+    if (max <= min) return []
+
+    const length = Math.abs(max - min);
+    let result = []
+    for (let i = 0, num = min; i < length; i++, num++) {
+        result = [...result, num]
+    }
+    return result
+}
+
+const numbers = arrayFromRange(1, 4);
+console.log(numbers); // Should display [1, 2, 3, 4]
+
+const negativeNumbers = arrayFromRange(-10, -8);
+console.log(negativeNumbers); // Should display [-10, -9, -8]
+
+
+// Exercise 3
+// Need to create function which display the max number from the array
+const numbers = [1, 2, 3, 4, 5];
+const maxNumber = getMax(numbers);
+
+console.log(maxNumber) // should display max number from the array 5
+
+function getMax(arr) {
+    let maxVal = 0 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= maxVal) maxVal = arr[i]
+    }
+    return maxVal
+}
+
+// Exercise 4
+
+let movies = [
+    {title: 'a', year: 2019, rating: 4.7},
+    {title: 'b', year: 2018, rating: 4.4},
+    {title: 'c', year: 2019, rating: 4.3},
+    {title: 'd', year: 2019, rating: 3.9},
+]
+
+// Ruturn all movies in 2019 with rating > 4
+// Sort them by their rating in descending order
+// Pick their title 'a', 'b' ...
+// So, the result should be ["a", "c"]
+
+const rating = 4
+const year = 2019
+const result = movies
+    .filter((movie) => movie.year === year && movie.rating > rating)
+    .sort((a, b) => b.rating - a.rating )
+    .map((movie) => movie.title)
+
+console.log('result = ', result)
