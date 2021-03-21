8b8c294181ec69240ba0c4d90b2984e106312cf8
Julia Kurianova <julia.kurianova@gmail.com>
2021-03-21 12:23:30 -0400

    Add Lesson-4 part-2

--- Lesson-4/lesson-4-part-2.js
+++ Lesson-4/lesson-4-part-2.js
@@ -2,49 +2,54 @@
 // Write a function which should return sum of all numbers inside array
 const numbers = [1, -1, 2, 3];
 
-function sum(numbers) {
+// function sum1(numbers) {
+//     let sum = 0
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum = sum + numbers[i]
+//     }
+//     return sum
+// }
+
+function sum2(numbers) {
     let sum = 0
-    for (let i = 0; i < numbers.length; i++) {
-        sum = sum + numbers[i]
-    }
+    numbers.forEach((number) => {
+        sum = sum + number
+    })
     return sum
 }
 
-console.log(sum(numbers)); // 5
-
-// console.log(numbers.reduce((sum, number) => sum + number))
+// console.log('sum with for: ', sum1(numbers));
+console.log('sum with forEach: ', sum2(numbers));
+// console.log('sum with reduce: ', numbers.reduce((sum, number) => sum + number))
 
 // Exercise 2
-// When you run arrayFromRange function should dispayd numbers from min to max 
+// When you run arrayFromRange function should dispayed numbers from min to max 
 // Also this function should accept negative numbers
 function arrayFromRange(min, max) {
     if (max <= min) return []
 
-    const length = Math.abs(max - min);
+    const length = max - min;
     let result = []
-    for (let i = 0, num = min; i < length; i++, num++) {
+    for (let i = 0, num = min; i <= length; i++, num++) {
         result = [...result, num]
     }
-    return result
+    result.forEach((num) => console.log(num))
 }
 
-const numbers = arrayFromRange(1, 4);
-console.log(numbers); // Should display [1, 2, 3, 4]
-
-const negativeNumbers = arrayFromRange(-10, -8);
-console.log(negativeNumbers); // Should display [-10, -9, -8]
+arrayFromRange(1, 4)
+arrayFromRange(-10, -8)
 
 
 // Exercise 3
-// Need to create function which display the max number from the array
+// Need to create function which displays the max number from the array
 const numbers = [1, 2, 3, 4, 5];
 const maxNumber = getMax(numbers);
 
 console.log(maxNumber) // should display max number from the array 5
 
 function getMax(arr) {
-    let maxVal = 0 
-    for (let i = 0; i < arr.length; i++) {
+    let maxVal = arr[0] 
+    for (let i = 1; i < arr.length; i++) {
         if (arr[i] >= maxVal) maxVal = arr[i]
     }
     return maxVal
@@ -59,7 +64,7 @@ let movies = [
     {title: 'd', year: 2019, rating: 3.9},
 ]
 
-// Ruturn all movies in 2019 with rating > 4
+// Return all movies in 2019 with rating > 4
 // Sort them by their rating in descending order
 // Pick their title 'a', 'b' ...
 // So, the result should be ["a", "c"]
